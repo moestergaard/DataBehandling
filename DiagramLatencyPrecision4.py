51fbdb72a723d2500d94a1e3ba96e2ddc22b5356
Martin Oestergaard <martin.malmkjaer.oestergaard@gmail.com>
2023-05-23 13:47:04 +0200

    Changes to the diagram

--- DiagramLatencyPrecision4.py
+++ DiagramLatencyPrecision4.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Define the data points
 points1 = [
@@ -23,16 +24,11 @@ x_values2 = [point[0] for point in points2]
 y_values2 = [point[1] for point in points2]
 colors2 = [point[2] for point in points2]
 
-# Create the figure and axis objects
-# fig, ax = plt.subplots()
 
 # Create two subplots with shared y-axis
 f, (ax, ax2) = plt.subplots(1, 2, sharey=True, facecolor='w')
 
 # Set the y-axis labels
-
-ax2.set_yticks([1, 2, 3])
-ax2.set_yticklabels([])
 ax.set_yticks([1, 2, 3])
 ax.set_yticklabels(['1', '2', '3'])
 
@@ -44,36 +40,32 @@ for x, y, color in zip(x_values2, y_values2, colors2):
     ax2.scatter(x, y, color=color)
 
 # Set the x-axis limits for each subplot
-ax.set_xlim(0, 25)
-ax2.set_xlim(125, 144)
+ax.set_xlim(5, 25)
+ax2.set_xlim(125, 145)
+
+# Set the x-axis labels
+ax.set_xticks([6, 12, 18, 24])
+ax.set_xticklabels(['6', '12', '18', '24'])
+ax2.set_xticks([126, 132, 138, 144])
+ax2.set_xticklabels(['126', '132', '138', '144'])
 
 # Hide the spines between ax and ax2
 ax.spines['right'].set_visible(False)
 ax2.spines['left'].set_visible(False)
 ax.yaxis.tick_left()
-# ax.tick_params(labelright='off')
 ax2.yaxis.tick_right()
-# ax2.tick_params(labelright='off')
 
 # Set plot title and labels
-ax.set_title('Data Points')
-ax.set_xlabel('x-axis')
-ax.set_ylabel('y-axis')
-
-# Add broken axes
-# ax.set_ylim(-1.2, 1.2)
-# ax2.set_ylim(-1.2, 1.2)
-# ax.spines['top'].set_visible(False)
-# ax2.spines['top'].set_visible(False)
-
-# Create the gap in the x-axis between 24 and 126
-# d1 = .02  # how big to make the diagonal lines in axes coordinates
-# kwargs1 = dict(transform=ax.transAxes, color='k', clip_on=False)
-# ax.plot((24-d1, 24+d1), (-d1, +d1), **kwargs1)
-# ax.plot((24-d1, 24+d1), (1-d1, 1+d1), **kwargs1)
-# kwargs2 = dict(transform=ax2.transAxes, color='k', clip_on=False)
-# ax2.plot((24-d1, 24+d1), (-d1, +d1), **kwargs2)
-# ax2.plot((24-d1, 24+d1), (1-d1, 1+d1), **kwargs2)
+plt.suptitle('Wi-Fi Scanninger', fontsize=12)
+# Add title for the x-axis in the middle
+f.text(0.5, 0.01, 'Sekunder', ha='center', fontsize=10)
+# plt.xlabel('Sekunder', fontsize=10, ha='center')
+# Add title for the x-axis in the middle
+# ax.set_xlabel('Sekunder', ha='center')
+# ax.xaxis.set_label_coords(0.5, -0.2)
+#set_title('Wi-Fi Scanninger')
+# ax.set_xlabel('')
+ax.set_ylabel('Antal på hinanden følgende\n detekteringer før skift')
 
 d = .015  # how big to make the diagonal lines in axes coordinates
 # arguments to pass plot, just so we don't keep repeating them
@@ -84,11 +76,6 @@ ax.plot((1-d, 1+d), (1-d, 1+d), **kwargs)
 kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
 ax2.plot((-d, +d), (1-d, 1+d), **kwargs)
 ax2.plot((-d, +d), (-d, +d), **kwargs)
-
-# Manually set x-axis tick positions and limits
-# x_ticks = [6, 12, 18, 24, 126, 132, 138, 144]
-# plt.xticks(x_ticks)
-# plt.xlim(x_ticks[0], x_ticks[-2])  # Exclude the last tick position from the limit
 
 # Display the plot
 plt.show()
