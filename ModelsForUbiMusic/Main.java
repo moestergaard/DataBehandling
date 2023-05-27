0f4cbf9941bb74e1dad622b44566e0b95ad62c70
Martin Oestergaard <martin.malmkjaer.oestergaard@gmail.com>
2023-05-27 21:25:05 +0200

    After cleanup in files

---
+++ ModelsForUbiMusic/Main.java
@@ -0,0 +1,19 @@
+package ModelsForUbiMusic;
+public class Main {
+
+    public static void main(String[] args) {
+
+        // create a new instance of the SVM classifier
+        SVMClassifier svm = new SVMClassifier();
+
+
+        // do something with the classifier, for example, load a model from a file
+        svm.loadModel("svm_model.pkl");
+
+        // use the classifier to predict the class label of a new data instance
+        double[] newData = {1.2, 3.4, 5.6, 7.8};
+        // double predictedLabel = svm.predict(newData);
+
+        // System.out.println("Predicted label: " + predictedLabel);
+    }
+}
